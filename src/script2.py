1e27c4d50da428a4e2ffafb463adc4cb57c824ee
payne <antilevski@tut.by>
2022-10-18 17:22:00 +0300

    script2 rewrote to async

--- src/script2.py
+++ src/script2.py
@@ -1,10 +1,11 @@
+import asyncio
 import re
 from typing import Any
 
+import aiohttp
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-from tqdm import tqdm
 
 from utils import get_nums
 from utils import write_data_to_json
@@ -23,53 +24,57 @@ def get_cities_id(url: str, headers: dict[str, Any]) -> list[str]:
     return all_cities
 
 
-def get_all_links(
-    all_cities: list[str], url: str, headers: dict[str, Any]
-) -> list[str]:
+async def get_all_links(idx, all_links, url, headers):
     """Getting all links of the shops"""
-    all_links = []
-    for city in tqdm(all_cities):
-        params = {"CITY_ID": city}
-        response = requests.post(url=url, headers=headers, params=params)
-        soup = BeautifulSoup(response.text, "lxml")
-        res = soup.find_all("a", class_="btn btn-blue")
-        for item in res:
-            all_links.append("https://www.som1.ru" + item["href"])
-    return all_links
+
+    params = {"CITY_ID": idx}
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url=url, headers=headers, params=params) as resp:
+            soup = BeautifulSoup(await resp.text(), "lxml")
+            res = soup.find_all("a", class_="btn btn-blue")
+            for item in res:
+                all_links.append("https://www.som1.ru" + item["href"])
 
 
-def parse_data(all_links: list[str], headers: dict[str, Any]) -> list[dict]:
+async def parse_data(
+    link: list[str], headers: dict[str, Any], result_json
+) -> list[dict]:
     """Getting all needed data from provided links"""
-    result_json = []
-    for link in tqdm(all_links):
-        response = requests.get(url=link, headers=headers)
-        soup = BeautifulSoup(response.text, "lxml")
-        description = soup.find("table", class_="shop-info-table").find_all("tr")
-        results = [item.find_all("td")[-1].text for item in description]
-        address, tel, hours = [*results]
-        tel = get_nums(tel)
-        name = soup.select(".page-body > div:nth-child(1) > h1:nth-child(1)")[0].text
-        cords = soup.select("body > script:nth-child(82)")[0].text
-        result = re.findall(r"[\d]+[.,\d]+", cords)
-        cords = [float(item) for item in result]
-        result = {
-            "address": address,
-            "latlon": cords,
-            "name": name,
-            "phones": tel,
-            "working_hours": hours,
-        }
-        result_json.append(result)
-    return result_json
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url=link, headers=headers) as resp:
+            soup = BeautifulSoup(await resp.text(), "lxml")
+            description = soup.find("table", class_="shop-info-table").find_all("tr")
+            results = [item.find_all("td")[-1].text for item in description]
+            address, tel, hours = [*results]
+            tel = get_nums(tel)
+            name = soup.select(".page-body > div:nth-child(1) > h1:nth-child(1)")[
+                0
+            ].text
+            cords = soup.select("body > script:nth-child(82)")[0].text
+            result = re.findall(r"[\d]+[.,\d]+", cords)
+            cords = [float(item) for item in result]
+            result = {
+                "address": address,
+                "latlon": cords,
+                "name": name,
+                "phones": tel,
+                "working_hours": hours,
+            }
+            result_json.append(result)
 
 
 def main() -> None:
     url = "https://www.som1.ru/shops/"
     headers = {"user-agent": UserAgent().random}
     all_cities = get_cities_id(url, headers)
-    all_links = get_all_links(all_cities, url, headers)
-    data = parse_data(all_links, headers)
-    write_data_to_json(data, "task2.json")
+    all_links = []
+    task = [get_all_links(city, all_links, url, headers) for city in all_cities]
+    asyncio.run(asyncio.wait(task))
+    result_json = []
+    task2 = [parse_data(link, headers, result_json) for link in all_links]
+    asyncio.run(asyncio.wait(task2))
+    write_data_to_json(result_json, "async_task2.json")
 
 
 if __name__ == "__main__":
