13f6f99f324503622c48a30d021d254895aa520b
swankyalex <72443505+swankyalex@users.noreply.github.com>
2022-10-18 18:01:05 +0300

    Scripts 1, 2 redone to OOP and async (#2)
    * script1 rewrote to OOP and async
    
    * script2 rewrote to async
    
    * script2 redone to OOP

--- src/script2.py
+++ src/script2.py
@@ -1,74 +1,87 @@
+import asyncio
 import re
-from typing import Any
 
+import aiohttp
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-from tqdm import tqdm
 
 from utils import get_nums
 from utils import write_data_to_json
 
 
-def get_cities_id(url: str, headers: dict[str, Any]) -> list[str]:
-    """Get all cities id from site"""
-    response = requests.get(url=url, headers=headers)
-    soup = BeautifulSoup(response.text, "lxml")
-    soup = soup.find_all("div", class_="cities-container")
-    all_cities = []
-    for item in soup:
-        result = item.find_all("label")
-        for x in result:
-            all_cities.append(x["id"])
-    return all_cities
+class Parser:
+    def __init__(self, link):
+        self.url = link
+        self.headers = {"user-agent": UserAgent().random}
+        self.city_idxs = []
+        self.all_links = []
+        self.data = []
 
-
-def get_all_links(
-    all_cities: list[str], url: str, headers: dict[str, Any]
-) -> list[str]:
-    """Getting all links of the shops"""
-    all_links = []
-    for city in tqdm(all_cities):
-        params = {"CITY_ID": city}
-        response = requests.post(url=url, headers=headers, params=params)
+    def get_cities_id(self):
+        """Get all cities id from site"""
+        response = requests.get(url=self.url, headers=self.headers)
         soup = BeautifulSoup(response.text, "lxml")
-        res = soup.find_all("a", class_="btn btn-blue")
-        for item in res:
-            all_links.append("https://www.som1.ru" + item["href"])
-    return all_links
+        soup = soup.find_all("div", class_="cities-container")
+        for item in soup:
+            result = item.find_all("label")
+            for x in result:
+                self.city_idxs.append(x["id"])
 
+    async def get_all_links(self, idx):
+        """Getting all links of the shops"""
+        params = {"CITY_ID": idx}
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                url=self.url, headers=self.headers, params=params
+            ) as resp:
+                soup = BeautifulSoup(await resp.text(), "lxml")
+                res = soup.find_all("a", class_="btn btn-blue")
+                for item in res:
+                    self.all_links.append("https://www.som1.ru" + item["href"])
 
-def parse_data(all_links: list[str], headers: dict[str, Any]) -> list[dict]:
-    """Getting all needed data from provided links"""
-    result_json = []
-    for link in tqdm(all_links):
-        response = requests.get(url=link, headers=headers)
-        soup = BeautifulSoup(response.text, "lxml")
-        description = soup.find("table", class_="shop-info-table").find_all("tr")
-        results = [item.find_all("td")[-1].text for item in description]
-        address, tel, hours = [*results]
-        tel = get_nums(tel)
-        name = soup.select(".page-body > div:nth-child(1) > h1:nth-child(1)")[0].text
-        cords = soup.select("body > script:nth-child(82)")[0].text
-        result = re.findall(r"[\d]+[.,\d]+", cords)
-        cords = [float(item) for item in result]
-        result = {
-            "address": address,
-            "latlon": cords,
-            "name": name,
-            "phones": tel,
-            "working_hours": hours,
-        }
-        result_json.append(result)
-    return result_json
+    async def parse_data(self, link):
+        """Getting all needed data from provided links"""
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=link, headers=self.headers) as resp:
+                soup = BeautifulSoup(await resp.text(), "lxml")
+                description = soup.find("table", class_="shop-info-table").find_all(
+                    "tr"
+                )
+                results = [item.find_all("td")[-1].text for item in description]
+                address, tel, hours = results
+                tel = get_nums(tel)
+                name = soup.select(".page-body > div:nth-child(1) > h1:nth-child(1)")[
+                    0
+                ].text
+                cords = soup.select("body > script:nth-child(82)")[0].text
+                result = re.findall(r"[\d]+[.,\d]+", cords)
+                cords = [float(item) for item in result]
+                result = {
+                    "address": address,
+                    "latlon": cords,
+                    "name": name,
+                    "phones": tel,
+                    "working_hours": hours,
+                }
+                self.data.append(result)
+
+    def runner(self):
+        print(f"Start parsing data from {self.url}")
+        self.get_cities_id()
+        print("City indexes received")
+        tasks = [self.get_all_links(idx) for idx in self.city_idxs]
+        asyncio.run(asyncio.wait(tasks))
+        print("All links received")
+        tasks2 = [self.parse_data(link) for link in self.all_links]
+        asyncio.run(asyncio.wait(tasks2))
+        return self.data
 
 
 def main() -> None:
     url = "https://www.som1.ru/shops/"
-    headers = {"user-agent": UserAgent().random}
-    all_cities = get_cities_id(url, headers)
-    all_links = get_all_links(all_cities, url, headers)
-    data = parse_data(all_links, headers)
+    parser = Parser(url)
+    data = parser.runner()
     write_data_to_json(data, "task2.json")
 
 
